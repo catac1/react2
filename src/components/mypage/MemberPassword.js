55f6ce107c3b8ead87871d129ac583df2d45e271
kst <kst@users.noreply.github.com>
2026-04-23 16:20:31 +0900

    Password

--- src/components/mypage/MemberPassword.js
+++ src/components/mypage/MemberPassword.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import { Button, Form, Input } from 'antd';
+import React, { useEffect } from 'react';
+import api from '../../api/axios';
+import { useNavigate } from 'react-router-dom';
 
 const MemberPassword = () => {
+
+    const navigate = useNavigate();
+    
+    const onFinish = async (values) => {
+        const url = `/member/password.json`;
+        const { data } = await api.put(url, values);
+        console.log(data);
+        if (data.status === 200) {
+            alert("암호 변경 완료");
+            navigate(-1);
+        } else {
+            alert("암호 변경 실패");
+        }
+    };
+
     return (
         <div>
             <h1>암호변경</h1>
+            <Form onFinish={onFinish}>
+                <Form.Item
+                    label="이전 암호"
+                    name="oldPassword"
+                    rules={[{ required: true }]}
+                >
+                    <Input.Password label />
+                </Form.Item>
+                <Form.Item
+                    label="새 암호"
+                    name="newPassword"
+                    rules={[{ required: true }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="암호 확인"
+                    name="password2"
+                    dependencies={['password']}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(new Error('The new password that you entered do not match!'));
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">암호 변경</Button>
+                </Form.Item>
+            </Form>
         </div>
     );
 };
